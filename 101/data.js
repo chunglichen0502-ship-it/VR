b417a2f9a2ed828c43e95d9db6836d4c6e1ea9f0
chunglichen0502-ship-it <chunglichen0502@gmail.com>
2025-11-21 11:45:28 +0800

    add

---
+++ 101/data.js
@@ -0,0 +1,271 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-101",
+      "name": "101廣場(白天)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.1294425654143616,
+        "pitch": -0.019243980442373854,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2002966538040667,
+          "pitch": 0.10652369543608664,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.950978788666811,
+          "pitch": 0.15312006997262628,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.2036406510213311,
+          "pitch": 0.2716136265663813,
+          "rotation": 3.141592653589793,
+          "target": "1-101"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2466138071155637,
+          "pitch": -0.11335484412855124,
+          "title": "<div>LOVE 台北之愛 台灣之愛</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7313973558942877,
+          "pitch": -0.1402277406430379,
+          "title": "Rebirth",
+          "text": "以台北101大樓電梯退役鋼纜打造而成，藝術家將冷硬的廢棄鋼纜轉化為線條柔軟、象徵生命初始之胚胎造型雕塑。"
+        }
+      ]
+    },
+    {
+      "id": "1-101",
+      "name": "101廣場(晚上)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11528454923124798,
+          "pitch": 0.26641967435906544,
+          "rotation": 3.141592653589793,
+          "target": "0-101"
+        },
+        {
+          "yaw": 0.12576513211701013,
+          "pitch": 0.0921842421407284,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.635651813702479,
+          "pitch": 0.20843959906632037,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8090271592394682,
+          "pitch": -0.3857040771682474,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.919817271815215,
+          "pitch": -0.5720788268965507,
+          "title": "Breeze",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0674680551205533,
+          "pitch": 0.1870891808909363,
+          "rotation": 0,
+          "target": "1-101"
+        },
+        {
+          "yaw": 2.2168329102445243,
+          "pitch": 0.14480093660111493,
+          "rotation": 11.780972450961727,
+          "target": "0-101"
+        },
+        {
+          "yaw": 0.9248473457157438,
+          "pitch": 0.20877820397423363,
+          "rotation": 7.0685834705770345,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "百貨大樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8160625298197033,
+        "pitch": -0.18306286378516745,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4694743421216243,
+          "pitch": 0.34628937343295263,
+          "rotation": 5.497787143782138,
+          "target": "0-101"
+        },
+        {
+          "yaw": 2.1631826182323604,
+          "pitch": 0.18131763046002547,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "購物中心",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.25467559493386815,
+        "pitch": 0.07173225056196841,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31109785102656673,
+          "pitch": 0.16254081262353992,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.3010515895040875,
+          "pitch": 0.6357768446318453,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "101",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
