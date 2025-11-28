88a66453b7c3ccbd92bcd7636bc450512ccb7a6c
chunglichen0502-ship-it <chunglichen0502@gmail.com>
2025-11-28 12:00:31 +0800

    efwufhio

---
+++ watch2/data.js
@@ -0,0 +1,231 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-101",
+      "name": "101百貨大樓手扶梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7927431016311743,
+        "pitch": 0.10123798585614807,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9102374106698434,
+          "pitch": -0.1704603869224517,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "觀景台室內",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.5784150651250783,
+        "pitch": 0.19618543345554684,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7654108159923716,
+          "pitch": 0.19284994795011023,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.9942255546474073,
+          "pitch": 0.3411175641415678,
+          "rotation": 7.0685834705770345,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.8481276775224682,
+          "pitch": 0.38109012408160936,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.589152849360973,
+          "pitch": 0.8181963029049868,
+          "rotation": 7.0685834705770345,
+          "target": "0-101"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "風阻尼球",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3139215641079254,
+          "pitch": 0.3298281139785484,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "觀景台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.824263052910535,
+        "pitch": 0.3548863876188868,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0605499145685577,
+          "pitch": 0.9291125201863331,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.4369213127568923,
+          "pitch": 0.8854874211359274,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "晚上觀景台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.3699930175140622,
+        "pitch": -0.04595572266736525,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.721999049138054,
+          "pitch": 0.028614288674710053,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.7384533210144753,
+          "pitch": -0.3345004273201475,
+          "rotation": 10.995574287564278,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
